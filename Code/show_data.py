92f9da558ca31dd877af160c0248022fa976d5e2
LegoOlegGelo <leviter517@gmail.com>
2023-09-29 12:16:22 +0300

    show data script implemented

--- Code/show_data.py
+++ Code/show_data.py
@@ -1,26 +1,41 @@
+from sklearn.neighbors import KNeighborsRegressor
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import cv2
 
 from dataloader import import_dataset_from_file
 
 
-maps = []
-images = []
+fig, axs = plt.subplots(2, 3)
+axs = axs.flatten()
 
-for i in range(1):
-    df = import_dataset_from_file(f'../Data/Map_{i+1}.txt')
-    maps.append(df)
+for j in range(5):
+    # loading dataset
+    print(f'Loading dataset #{j+1}')
+    df = import_dataset_from_file(f'../Data/Map_{j+1}.txt')
+    XY, z = df[['x', 'y']].values, df['z'].values
 
-    unique_y = df['y'].unique()
-    image = []
+    # sizes
+    xmin, ymin, xmax, ymax = XY[:, 0].min(), XY[:, 1].min(), XY[:, 0].max(), XY[:, 1].max()
 
-    for y in unique_y:
-        row = df[df['y'] == y]['z']
-        image.append(row)
-        print(row.shape)
-    
-    images.append(np.array(image))
+    # knn training
+    print('kNN training')
+    knn = KNeighborsRegressor(n_neighbors=4, weights='distance', metric='l2')
+    knn.fit(XY, z)
 
-# print([img.shape for img in images])
+    # showing
+    h, w = (1000, 1000) # height X width
+    img = np.zeros((h, w))
+    y_range = np.linspace(ymin, ymax, h)
 
+    for i in range(h):
+        y = y_range[i]
+        xy = np.array([(x, y) for x in np.linspace(xmin, xmax, w)])
+        pred = knn.predict(xy)
+        img[i, :] = pred
 
+    axs[j].set_title(f'Map {j+1}')
+    axj = axs[j].imshow(img, cmap='gist_rainbow')
+
+plt.show()
