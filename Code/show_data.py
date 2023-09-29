6b004a5f7dbcc16ec868520000e10741010d1a00
LegoOlegGelo <leviter517@gmail.com>
2023-09-29 17:18:07 +0300

    removed unused imports

--- Code/show_data.py
+++ Code/show_data.py
@@ -2,7 +2,6 @@ from sklearn.neighbors import KNeighborsRegressor
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import cv2
 
 from dataloader import import_dataset_from_file
 
